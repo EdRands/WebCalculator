d9ab2adfc968088000bf8d0cc934d44b8591907f
Ed Rands <erands@gmail.com>
2020-01-02 15:42:12 -0800

    Fix for consistency.

--- tests/cypress/integration/appearance.spec.js
+++ tests/cypress/integration/appearance.spec.js
@@ -1,4 +1,4 @@
-describe("The Calculator App", function() {
+describe("The Calculator App", () => {
   it("successfully loads", () => {
     cy.visit("/");
   });
@@ -15,7 +15,7 @@ describe("The Calculator App", function() {
       .and("equal", "UTF-8");
   });
 
-  it("has a title", function() {
+  it("has a title", () => {
     cy.title().should("include", "Calculator");
   });
 
@@ -26,121 +26,121 @@ describe("The Calculator App", function() {
     cy.get("#display").should("have.value", "0");
   });
 
-  it("shows the button for number one", function() {
+  it("shows the button for number one", () => {
     cy.get("#number1-button")
       .should("be.visible")
       .and("contain.text", "1");
   });
 
-  it("shows the button for number two", function() {
+  it("shows the button for number two", () => {
     cy.get("#number2-button")
       .should("be.visible")
       .and("contain.text", "2");
   });
 
-  it("shows the button for number three", function() {
+  it("shows the button for number three", () => {
     cy.get("#number3-button")
       .should("be.visible")
       .and("contain.text", "3");
   });
 
-  it("shows the button for number four", function() {
+  it("shows the button for number four", () => {
     cy.get("#number4-button")
       .should("be.visible")
       .and("contain.text", "4");
   });
 
-  it("shows the button for number five", function() {
+  it("shows the button for number five", () => {
     cy.get("#number5-button")
       .should("be.visible")
       .and("contain.text", "5");
   });
 
-  it("shows the button for number six", function() {
+  it("shows the button for number six", () => {
     cy.get("#number6-button")
       .should("be.visible")
       .and("contain.text", "6");
   });
 
-  it("shows the button for number seven", function() {
+  it("shows the button for number seven", () => {
     cy.get("#number7-button")
       .should("be.visible")
       .and("contain.text", "7");
   });
 
-  it("shows the button for number eight", function() {
+  it("shows the button for number eight", () => {
     cy.get("#number8-button")
       .should("be.visible")
       .and("contain.text", "8");
   });
 
-  it("shows the button for number nine", function() {
+  it("shows the button for number nine", () => {
     cy.get("#number9-button")
       .should("be.visible")
       .and("contain.text", "9");
   });
 
-  it("shows the button for number zero", function() {
+  it("shows the button for number zero", () => {
     cy.get("#number0-button")
       .should("be.visible")
       .and("contain.text", "0");
   });
 
-  it("shows the button for a decimal point", function() {
+  it("shows the button for a decimal point", () => {
     cy.get("#decimal-button")
       .should("be.visible")
       .and("contain.text", ".");
   });
 
-  it("shows the button for addition", function() {
+  it("shows the button for addition", () => {
     cy.get("#add-button")
       .should("be.visible")
       .and("contain.text", "+");
   });
 
-  it("shows the button for subtraction", function() {
+  it("shows the button for subtraction", () => {
     cy.get("#subtract-button")
       .should("be.visible")
       .and("contain.text", "−");
   });
 
-  it("shows the button for multiplication", function() {
+  it("shows the button for multiplication", () => {
     cy.get("#multiply-button")
       .should("be.visible")
       .and("contain.text", "×");
   });
 
-  it("shows the button for division", function() {
+  it("shows the button for division", () => {
     cy.get("#divide-button")
       .should("be.visible")
       .and("contain.text", "÷");
   });
 
-  it("shows the button for totaling", function() {
+  it("shows the button for totaling", () => {
     cy.get("#equals-button")
       .should("be.visible")
       .and("contain.text", "=");
   });
 
-  it("shows the button to toggle negative/positive", function() {
+  it("shows the button to toggle negative/positive", () => {
     cy.get("#negative-toggle-button")
       .should("be.visible")
       .and("contain.text", "±");
   });
 
-  it("shows the button to clear everything", function() {
+  it("shows the button to clear everything", () => {
     cy.get("#clear-all-button")
       .should("be.visible")
       .and("contain.text", "C");
   });
 
-  it("shows the button to clear the current entry", function() {
+  it("shows the button to clear the current entry", () => {
     cy.get("#clear-entry-button")
       .should("be.visible")
       .and("contain.text", "CE");
   });
 
-  it("shows the button to remove the last digit", function() {
+  it("shows the button to remove the last digit", () => {
     cy.get("#backspace-button")
       .should("be.visible")
       .and("contain.text", "←");
